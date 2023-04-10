a3b19605a23630897e85f0ae3bb187aa54436a52
Cameron <cameronjamesbishop@googlemail.com>
2023-04-10 15:46:10 +0000

    Basic game kinda works

--- yahtzee.rb
+++ yahtzee.rb
@@ -7,16 +7,17 @@
 }
 
 @counter = 0
-@rolled_dice = []
 @hand = []
+@held_dice = 5
+@rolled_dice = []
 
 def play
-    puts "'roll' the dice ('hold some' or 'hold all' after the first roll)"
+    puts "'roll' the dice ('hold' or 'hold all' after the first roll)"
     input = gets.chomp
     if @counter < 3
         if input == "roll"
             roll
-        elsif input == "hold some" && @counter > 0
+        elsif input == "hold" && @counter > 0
             hold
         elsif input == "hold all" && @counter > 0
             log_score
@@ -24,18 +25,28 @@ def play
             exit
         end
     end
+    if @counter == 0 && input == "roll"
+        log_score
+    end
 end
 
 def roll
-    @die.each_value { |dice| @rolled_dice << dice }
-    puts @rolled_dice.join(", ")
+    @rolled_dice = []
+    if @held_dice < 5
+        @held_dice.times { |x| @rolled_dice << rand(1..6)}
+        puts @rolled_dice.join(", ")
+    else 
+        @die.each_value { |dice| @rolled_dice << dice }
+        puts @rolled_dice.join(", ")
+    end    
     @counter += 1
     puts "A, B, C, D, E"
+    puts "Turn " + @counter.to_s + "\n"
     play
 end
 
 def hold
-    puts "Type in the dice you want to hold"
+    puts "\n Type in the dice you want to hold"
     input = gets.strip
     selection = input.split("")
     dice_key = {"A" => 0, "B" => 1, "C" => 2, "D" => 3, "E" => 4}
@@ -43,14 +54,13 @@ def hold
         @hand << @rolled_dice[dice_key[x].to_i]
     end 
     puts @hand.inspect
-#    @rolled_dice.map do |die|
-#       if 
-#       @hand << selection[die] 
-#    end
+    @held_dice -= @hand.length
+    puts "Turn " + @counter.to_s + "\n"
+    play
 end
 
 def log_score
-    puts "Feature coming soon"
+    puts "Out of rolls"
 end
 
 def score 
